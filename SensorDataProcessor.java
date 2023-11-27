aedd48937c9906ba6eab6f8746db305a0a18d3da
sxh01 <69437567+sxh01@users.noreply.github.com>
2023-11-27 22:10:51 +0300

    Update SensorDataProcessor.java Sarah Haddad 2006719
    Proper exception handling and better loop structure

--- SensorDataProcessor.java
+++ SensorDataProcessor.java
@@ -25,44 +25,25 @@ public class SensorDataProcessor {
     }
 // calculate data
 
-    public void calculate(double d) {
-        int i, j, k = 0;
-        double[][][] data2 = new double[data.length][data[0].length][data[0][0].length];
-        BufferedWriter out;
+     public void calculate(double divisor) {
+        double[][][] calculatedData = new double[data.length][data[0].length][data[0][0].length];
 
-// Write racing stats data into a file
-        try {
-            out = new BufferedWriter(new FileWriter("RacingStatsData.txt"));
-            for (i = 0; i < data.length; i++) {
-                for (j = 0; j < data[0].length; j++) {
-                    for (k = 0; k < data[0][0].length; k++) {
-                        data2[i][j][k] = data[i][j][k] / d
-                                - Math.pow(limit[i][j], 2.0);
-                        if (average(data2[i][j]) > 10 && average(data2[i][j])
-                                < 50) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("RacingStatsData.txt"))) {
+            for (int i = 0; i < data.length; i++) {
+                for (int j = 0; j < data[0].length; j++) {
+                    for (int k = 0; k < data[0][0].length; k++) {
+                        calculatedData[i][j][k] = data[i][j][k] / divisor - Math.pow(limit[i][j], 2.0);
+
+                        double average = calculateAverage(calculatedData[i][j]);
+
+                        if (average > 10 && average < 50) {
                             break;
-                        } else if (Math.max(data[i][j][k], data2[i][j][k])
-                                > data[i][j][k]) {
+                        } else if (Math.max(data[i][j][k], calculatedData[i][j][k]) > data[i][j][k]) {
                             break;
-                        } else if (Math.pow(Math.abs(data[i][j][k]), 3)
-                                < Math.pow(Math.abs(data2[i][j][k]), 3)
-                                && average(data[i][j]) < data2[i][j][k] && (i + 1)
-                                * (j + 1) > 0) {
-                            data2[i][j][k] *= 2;
-                        } else {
-                            continue;
+                        } else if (Math.pow(Math.abs(data[i][j][k]), 3) < Math.pow(Math.abs(calculatedData[i][j][k]), 3)
+                                && calculateAverage(data[i][j]) < calculatedData[i][j][k] && (i + 1) * (j + 1) > 0) {
+                            calculatedData[i][j][k] *= 2;
                         }
                     }
                 }
             }
-            for (i = 0; i < data2.length; i++) {
-                for (j = 0; j < data2[0].length; j++) {
-                    out.write(data2[i][j] + "\t");
-                }
-            }
-            out.close();
-        } catch (Exception e) {
-            System.out.println("Error= " + e);
-        }
-    }
-}
